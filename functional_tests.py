72e716d5901d3639024ebdf12f7f2b4d3429742a
ekevgri <kevin.griffin@ericsson.com>
2015-08-25 09:50:02 +0100

    T using unittests

--- functional_tests.py
+++ functional_tests.py
@@ -2,10 +2,25 @@
 
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import unittest
 
-binary = FirefoxBinary('/usr/bin/firefox')
-browser = webdriver.Firefox(firefox_binary=binary)
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.binary = FirefoxBinary('/usr/bin/firefox')
+        self.browser = webdriver.Firefox(firefox_binary=self.binary)
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
 
 
